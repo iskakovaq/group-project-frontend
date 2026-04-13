a8933949d3950c30d54f54266ee0b1148e7e7689
iskakovaq <47221965+iskakovaq@users.noreply.github.com>
2026-04-14 00:56:05 +0500

    Delete lab8 directory

--- lab8/shop_back/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include  # include нужен для подключения api.urls
-
-urlpatterns = [
-    path('admin/', admin.site.urls),   # Админка
-    path('api/', include('api.urls')), # Подключаем API приложения 'api'
-]
